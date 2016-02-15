8b20be751ae11bd35f46e0ff3fd01b834ae72f02
fuuukai <caxa8.fukai@gmail.com>
2016-02-15 18:43:39 +0900

    変数i,nを追加

--- app/src/main/java/com/lifeistech/android/gitapplication/MainActivity.java
+++ app/src/main/java/com/lifeistech/android/gitapplication/MainActivity.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    int i;
+    int n;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
 }
